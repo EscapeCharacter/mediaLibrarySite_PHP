96e9a11a76a6014b06b045a53e1c7edcac5c61ab
Sagiv Lapkin <lapkins@USH10024.local>
2017-06-29 14:25:11 -0400

    Delete process.php

--- suggest.php
+++ suggest.php
@@ -1,4 +1,21 @@
 <?php 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$details = $_POST['details'];
+
+echo "<pre>";
+$email_body = '';
+$email_body .= 'Name ' . $name . "\n";
+$email_body .= 'Email ' . $email . "\n";
+$email_body .= 'Details ' . $details . "\n";
+echo $email_body;
+echo "</pre>";
+
+
+// To do: Send email
+header('location:thanks.php');
+?>
+
 $pageTitle = 'Suggest a Media Item';
 $section = 'suggest';
 
@@ -8,7 +25,7 @@ include("inc/header.php"); ?>
 	<div class="wrapper">
 		<h1>Suggest a Media Item</h1>
 		<p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
-		<form action="process.php" method="POST">
+		<form action="suggest.php" method="POST">
 		<table>
 			<tr>
 				<th><label for="name">Name</label></th>
---
+++ thanks.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = 'Thank you';
+$section = NULL;
+include('inc/header.php');
+?>
+
+<div class="section page">
+	<h1>Thank you</h1>
+	<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>
+</div>
+
+<?php include('inc/footer.php'); ?>
